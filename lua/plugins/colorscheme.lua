503e0d38d0f8ea1a5f78741d4dc8847dd4621aa5
Dmitrii Sorokin <sorokinda00@yandex.ru>
2025-02-02 14:39:36 +0300

    Switched to gruvbox.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,29 +1,29 @@
-return {
-  'folke/tokyonight.nvim',
-  lazy = false,
-  priority = 1000,
-  init = function()
-    -- Load the colorscheme here.
-    -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'tokyonight-night'
-
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
-  end,
-}
-
 -- return {
---   'ellisonleao/gruvbox.nvim',
---   priority = 1000, -- Make sure to load this before all the other start plugins.
+--   'folke/tokyonight.nvim',
+--   lazy = false,
+--   priority = 1000,
 --   init = function()
 --     -- Load the colorscheme here.
 --     -- Like many other themes, this one has different styles, and you could load
 --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
---     vim.o.background = 'dark'
---     vim.cmd.colorscheme 'gruvbox'
+--     vim.cmd.colorscheme 'tokyonight-night'
 --
 --     -- You can configure highlights by doing something like:
 --     vim.cmd.hi 'Comment gui=none'
 --   end,
 -- }
+
+return {
+  'ellisonleao/gruvbox.nvim',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  init = function()
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.o.background = 'dark'
+    vim.cmd.colorscheme 'gruvbox'
+
+    -- You can configure highlights by doing something like:
+    vim.cmd.hi 'Comment gui=none'
+  end,
+}
