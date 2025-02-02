bfd67e0766b95c976c8f2dc85e11349e145021e6
Dmitrii Sorokin <sorokinda00@yandex.ru>
2025-02-02 19:14:16 +0300

    Toggable conform.nvim

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,3 +1,4 @@
+vim.g.disable_autoformat = true
 return { -- Autoformat
   'stevearc/conform.nvim',
   event = { 'BufWritePre' },
@@ -15,35 +16,38 @@ return { -- Autoformat
   opts = {
     notify_on_error = true,
     format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
-      local lsp_format_opt
-      if disable_filetypes[vim.bo[bufnr].filetype] then
-        lsp_format_opt = 'never'
-      else
-        lsp_format_opt = 'fallback'
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
       end
-      return {
-        timeout_ms = 500,
-        lsp_format = lsp_format_opt,
-      }
+      return { timeout_ms = 500, lsp_format = 'fallback' }
     end,
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true,
+    }),
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Re-enable autoformat-on-save',
+    }),
     formatters_by_ft = {
       lua = { 'stylua' },
-      c = { 'clang_format' },
-      cpp = { 'clang_format' },
+      c = { 'clang-format' },
+      cpp = { 'clang-format' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
-    },
-    formatter = {
-      clang_format = {
-        prepend_args = { '--style=file', '--fallback-style=Microsoft' },
-      },
     },
   },
 }
