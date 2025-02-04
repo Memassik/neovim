8e78d5b684452b9ac0c94e4127bd7b0b0a5e428e
Dmitrii Sorokin <d.sorokin@aerodisk.ru>
2025-02-04 10:35:18 +0300

    Replaced back)

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -2,8 +2,8 @@ return {
   'neovim/nvim-lspconfig',
   event = { 'BufReadPre', 'BufNewFile' },
   dependencies = {
-    'saghen/blink.cmp',
     'j-hui/fidget.nvim',
+    'hrsh7th/cmp-nvim-lsp',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
   },
   config = function()
@@ -103,7 +103,7 @@ return {
     --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
     --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities())
+    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
     -- Enable the following language servers
     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
