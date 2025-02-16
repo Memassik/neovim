a07b183903f02634b499e128dbf6a9e5b87b674f
Dmitrii Sorokin <sorokinda00@yandex.ru>
2025-02-16 17:15:48 +0300

    Updated coloscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,19 @@
 -- return {
+--   'kepano/flexoki-neovim',
+--   lazy = false,
+--   priority = 1000,
+--   init = function()
+--     -- Load the colorscheme here.
+--     -- Like many other themes, this one has different styles, and you could load
+--     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+--     vim.cmd.colorscheme 'flexoki-dark'
+--
+--     -- You can configure highlights by doing something like:
+--     vim.cmd.hi 'Comment gui=none'
+--   end,
+-- }
+
+-- return {
 --   'folke/tokyonight.nvim',
 --   lazy = false,
 --   priority = 1000,
