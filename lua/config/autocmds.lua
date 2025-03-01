3a650a9b5b1fedd45faeed5d9472ca394ce77558
Dmitrii Sorokin <sorokinda00@yandex.ru>
2025-03-01 11:48:58 +0300

    Disabled cscope autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -73,11 +73,11 @@ vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
   end,
 })
 
-local group = vim.api.nvim_create_augroup("CscopeBuild", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { "*.c", "*.h" },
-  callback = function ()
-    vim.cmd("Cscope db build")
-  end,
-  group = group,
-})
+-- local group = vim.api.nvim_create_augroup("CscopeBuild", { clear = true })
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--   pattern = { "*.c", "*.h" },
+--   callback = function ()
+--     vim.cmd("Cscope db build")
+--   end,
+--   group = group,
+-- })
