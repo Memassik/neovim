8d1dbba4f91d35f3e19520b2d2f2c0fac2a3f860
Dmitrii Sorokin <d.sorokin@aerodisk.ru>
2025-03-28 18:10:20 +0300

    Added nvim-dap config

--- lua/configs/langs/rust.lua
+++ lua/configs/langs/rust.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.lang = function()
-    return {
+	return {
 		autostart = false,
 		imports = {
 			granularity = {
@@ -21,21 +21,75 @@ M.lang = function()
 end
 
 M.format = {
-    rust = { "rustfmt" },
+	rust = { "rustfmt" },
 }
 
 M.setup = function()
-    require("configs.lspconfig").defaults()
+	require("configs.lspconfig").defaults()
 
-    local lspconfig = require "lspconfig"
-    local lsp = require "configs.lspconfig"
-    local options = M.lang()
+	local dap = require("dap")
+	local lspconfig = require "lspconfig"
+	local lsp = require "configs.lspconfig"
+	local options = M.lang()
 
-    options.capabilities = vim.tbl_deep_extend('force', {}, lsp.capabilities, options.capabilities or {})
-    options.on_attach = lsp.on_attach
-    options.on_init = lsp.on_init
+	options.capabilities = vim.tbl_deep_extend('force', {}, lsp.capabilities, options.capabilities or {})
+	options.on_attach = lsp.on_attach
+	options.on_init = lsp.on_init
 
-    lspconfig.rust_analyzer.setup(options)
+	lspconfig.rust_analyzer.setup(options)
+
+	dap.adapters.gdb = {
+		type = "executable",
+		command = "gdb",
+		args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+	}
+
+	dap.configurations.rust = {
+		{
+			name = 'Run executable (GDB)',
+			type = 'gdb',
+			request = 'launch',
+			-- This requires special handling of 'run_last', see
+			-- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
+			program = function()
+				local path = vim.fn.input({
+					prompt = 'Path to executable: ',
+					default = vim.fn.getcwd() .. '/',
+					completion = 'file',
+				})
+
+				return (path and path ~= '') and path or dap.ABORT
+			end,
+		},
+		{
+			name = 'Run executable with arguments (GDB)',
+			type = 'gdb',
+			request = 'launch',
+			-- This requires special handling of 'run_last', see
+			-- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
+			program = function()
+				local path = vim.fn.input({
+					prompt = 'Path to executable: ',
+					default = vim.fn.getcwd() .. '/',
+					completion = 'file',
+				})
+
+				return (path and path ~= '') and path or dap.ABORT
+			end,
+			args = function()
+				local args_str = vim.fn.input({
+					prompt = 'Arguments: ',
+				})
+				return vim.split(args_str, ' +')
+			end,
+		},
+		{
+			name = 'Attach to process (GDB)',
+			type = 'gdb',
+			request = 'attach',
+			processId = require('dap.utils').pick_process,
+		},
+	}
 end
 
 return M
