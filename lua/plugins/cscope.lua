b257651380620a22731a4079980015c0bd9c1c54
Dmitrii Sorokin <d.sorokin@aerodisk.ru>
2025-04-11 11:02:59 +0300

    Added cscope again and TODO

---
+++ lua/plugins/cscope.lua
@@ -0,0 +1,9 @@
+return {
+	"dhananjaylatkar/cscope_maps.nvim",
+	dependencies = {
+		"ibhagwan/fzf-lua", -- optional [for picker="fzf-lua"]
+	},
+	opts = {
+		prefix = "<leader>cc", -- prefix to trigger maps
+	},
+}
