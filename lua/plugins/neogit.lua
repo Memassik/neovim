66fa40507269392a67b94134249740f170c282a5
Dmitrii Sorokin <sorokinda00@yandex.ru>
2025-05-04 14:21:34 +0300

    Fully removed lazygit.

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,8 +1,21 @@
 return {
 	"NeogitOrg/neogit",
+	cmd = {
+		"Neogit",
+		"NeogitCommit",
+		"NeogitLogCurrent",
+		"NeogitResetState",
+	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"sindrets/diffview.nvim",
 		"ibhagwan/fzf-lua", -- optional
+	},
+	opts = {
+		disable_context_highlighting = true,
+	},
+	keys = {
+		{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
+		{ "<leader>gf", "<cmd>NeogitLogCurrent<cr>", desc = "Git Current File History" },
 	},
 }
