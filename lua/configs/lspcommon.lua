04252b0d7133981388cfdf99c9312e431ec63b0e
Dmitrii Sorokin <d.sorokin@aerodisk.ru>
2025-05-12 15:58:30 +0300

    Removed annoying lsp auto highlight

--- lua/configs/lspcommon.lua
+++ lua/configs/lspcommon.lua
@@ -117,32 +117,32 @@ M.config = function()
 			map("<leader>ws", "<cmd>FzfLua lsp_live_workspace_symbols<cr>", "Workspace Symbols")
 			map("<leader>ca", vim.lsp.buf.code_action, "Code action")
 
-			local client = vim.lsp.get_client_by_id(event.data.client_id)
-			if
-				client
-				and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf)
-			then
-				local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
-				vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-					buffer = event.buf,
-					group = highlight_augroup,
-					callback = vim.lsp.buf.document_highlight,
-				})
-
-				vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-					buffer = event.buf,
-					group = highlight_augroup,
-					callback = vim.lsp.buf.clear_references,
-				})
-
-				vim.api.nvim_create_autocmd("LspDetach", {
-					group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
-					callback = function(event2)
-						vim.lsp.buf.clear_references()
-						vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
-					end,
-				})
-			end
+			-- local client = vim.lsp.get_client_by_id(event.data.client_id)
+			-- if
+			-- 	client
+			-- 	and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf)
+			-- then
+			-- 	local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+			-- 	vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+			-- 		buffer = event.buf,
+			-- 		group = highlight_augroup,
+			-- 		callback = vim.lsp.buf.document_highlight,
+			-- 	})
+			--
+			-- 	vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+			-- 		buffer = event.buf,
+			-- 		group = highlight_augroup,
+			-- 		callback = vim.lsp.buf.clear_references,
+			-- 	})
+			--
+			-- 	vim.api.nvim_create_autocmd("LspDetach", {
+			-- 		group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
+			-- 		callback = function(event2)
+			-- 			vim.lsp.buf.clear_references()
+			-- 			vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
+			-- 		end,
+			-- 	})
+			-- end
 
 			if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
 				map("<leader>uL", function()
