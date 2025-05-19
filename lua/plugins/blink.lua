46da9660fbded670c6375439fb97aabd57179d5b
Dmitrii Sorokin <d.sorokin@aerodisk.ru>
2025-05-19 13:49:56 +0300

    Removed blink-ripgrep.

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -18,7 +18,6 @@ return {
 			},
 			opts = {},
 		},
-		"mikavilpas/blink-ripgrep.nvim",
 	},
 	opts = {
 		cmdline = {
@@ -55,7 +54,7 @@ return {
 		},
 
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer", "ripgrep" },
+			default = { "lsp", "path", "snippets", "buffer" },
 			providers = {
 				buffer = {
 					opts = {
@@ -72,43 +71,6 @@ return {
 							return vim.fn.getcwd()
 						end,
 					},
-				},
-				ripgrep = {
-					module = "blink-ripgrep",
-					name = "Ripgrep",
-					opts = {
-						prefix_min_len = 4,
-						context_size = 5,
-						max_filesize = "1M",
-						project_root_marker = ".git",
-						project_root_fallback = true,
-						search_casing = "--ignore-case",
-						additional_rg_options = {},
-						fallback_to_regex_highlighting = true,
-						ignore_paths = { "tags", "cscope*" },
-						additional_paths = {},
-						toggles = {
-							-- The keymap to toggle the plugin on and off from blink
-							-- completion results. Example: "<leader>tg"
-							on_off = nil,
-						},
-						future_features = {
-							issue185_workaround = true,
-							backend = {
-								use = "gitgrep-or-ripgrep",
-							},
-						},
-						debug = false,
-					},
-					transform_items = function(_, items)
-						for _, item in ipairs(items) do
-							-- example: append a description to easily distinguish rg results
-							item.labelDetails = {
-								description = "(rg)",
-							}
-						end
-						return items
-					end,
 				},
 			},
 		},
