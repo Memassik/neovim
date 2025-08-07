87fc07a4f85b37c3887bbd3dbef9824e963dcfde
Dmitrii Sorokin <d.sorokin@aerodisk.ru>
2025-08-07 08:30:53 +0300

    Another small changes to neogit.

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -9,12 +9,10 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"sindrets/diffview.nvim",
-		"ibhagwan/fzf-lua", -- optional
+		"ibhagwan/fzf-lua",
 	},
 	opts = {
 		disable_context_highlighting = true,
-		disable_insert_on_commit = true,
-		process_spinner = true,
 	},
 	keys = {
 		{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
