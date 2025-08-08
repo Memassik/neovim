fad598a517cd9b08e9be053cf5e6e84242db6dc1
Dmitrii Sorokin <d.sorokin@aerodisk.ru>
2025-08-08 12:21:16 +0300

    Changed config for autopairs. Added nvim-surround.

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -4,5 +4,19 @@ return {
 	config = true,
 	opts = {
 		map_cr = true,
+		enable_check_bracket_line = false,
+		fast_wrap = {
+			map = "<M-e>",
+			chars = { "{", "[", "(", '"', "'" },
+			pattern = [=[[%'%"%>%]%)%}%,]]=],
+			end_key = "$",
+			before_key = "h",
+			after_key = "l",
+			cursor_pos_before = true,
+			keys = "qwertyuiopzxcvbnmasdfghjkl",
+			manual_position = true,
+			highlight = "Search",
+			highlight_grey = "Comment",
+		},
 	},
 }
