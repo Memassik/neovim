01d16a2a9530582e5c4d5399928cba1126111e7d
Dmitrii Sorokin <d.sorokin00@yandex.com>
2025-08-13 12:17:06 +0300

    Changed default settings.

--- lua/options.lua
+++ lua/options.lua
@@ -80,8 +80,6 @@ opt.wrap = false -- Disable line wrap
 g.markdown_recommended_style = 0
 
 -- disable some default providers
-g.loaded_node_provider = 0
-g.loaded_python3_provider = 0
 g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
 
