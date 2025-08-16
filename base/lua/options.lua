d8e2c3883ec58b7360af67ae03bd48dd52107070
Dmitrii Sorokin <d.sorokin00@yandex.com>
2025-08-16 19:10:04 +0300

    Removed nodejs provider, cause why do i need it?

--- base/lua/options.lua
+++ base/lua/options.lua
@@ -82,6 +82,7 @@ g.markdown_recommended_style = 0
 -- disable some default providers
 g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
+g.loaded_node_provider = 0
 
 -- USER DEFINED VARIABLES
 g.disable_autoformat = true
