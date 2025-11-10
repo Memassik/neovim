0a6e6b3d80d53c0d0e16d34ac4e11f8792b7e459
Dmitrii Sorokin <sorokinda00@yandex.ru>
2025-11-10 17:12:17 +0300

    Fixed <C-k> keymap in insert mode.

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -29,7 +29,23 @@ return {
 			-- <c-n>/<c-p> or <up>/<down>: Select next/previous item
 			-- <c-e>: Hide menu
 			-- <c-k>: Toggle signature help
-			preset = "default",
+			preset = "none",
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
+			["<C-y>"] = { "select_and_accept", "fallback" },
+
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+			["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+			["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+			["<Tab>"] = { "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+			["<C-S-K>"] = { "show_signature", "hide_signature", "fallback" },
 		},
 
 		appearance = {
