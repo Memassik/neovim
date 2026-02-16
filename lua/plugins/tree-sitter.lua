7b4a89e3651ab644f0845b627a7b94dbe934ae05
Dmitrii Sorokin <d.sorokin@aerodisk.ru>
2026-02-16 10:12:04 +0300

    nvim-treesitter: breaking update fixes.

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,38 +1,28 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPost", "BufNewFile" },
-	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+	lazy = false,
 	build = ":TSUpdate",
-	main = "nvim-treesitter.configs", -- Sets main module to use for opts
-	opts = {
-		ensure_installed = {
+	config = function()
+		local filetypes = {
+			"bash",
 			"c",
 			"cpp",
+			"diff",
+			"html",
 			"lua",
 			"luadoc",
-			"vim",
-			"vimdoc",
 			"markdown",
 			"markdown_inline",
-		},
-		highlight = {
-			enable = function()
-				return true
+			"query",
+			"vim",
+			"vimdoc",
+		}
+		require("nvim-treesitter").install(filetypes):wait(120000)
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = filetypes,
+			callback = function()
+				vim.treesitter.start()
 			end,
-			disable = function(lang, bufnr)
-				return vim.api.nvim_buf_line_count(bufnr) > 10000
-			end,
-			additional_vim_regex_highlighting = false,
-		},
-		indent = { enable = false },
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<leader>vv",
-				node_incremental = "<leader>vi",
-				scope_incremental = "<leader>vs",
-				node_decremental = "<leader>vd",
-			},
-		},
-	},
+		})
+	end,
 }
