fb134b3621da4cf2b58b404272d2a3867aee387f
Dmitrii Sorokin <sorokinda00@yandex.ru>
2026-04-13 11:35:17 +0300

    markdown: added toggle keymap, disabled by def.

--- plugin/render-markdown.lua
+++ plugin/render-markdown.lua
@@ -3,4 +3,14 @@ vim.pack.add({
 	"https://github.com/nvim-tree/nvim-web-devicons",
 	"https://github.com/MeanderingProgrammer/render-markdown.nvim",
 })
-require("render-markdown").setup({})
+
+require("render-markdown").setup({
+	completions = { lsp = { enabled = true } },
+})
+
+require("render-markdown").disable()
+
+local map = vim.keymap.set
+map("n", "<leader>um", function()
+	require("render-markdown").toggle()
+end, { desc = "render-markdown" })
